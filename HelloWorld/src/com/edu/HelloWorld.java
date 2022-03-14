f6769d900e84c7734813657bbaa914ca1c5f922e
admin <admin@YD-13>
2022-03-14 15:43:12 +0900

    세번째 커밋.

--- HelloWorld/src/com/edu/HelloWorld.java
+++ HelloWorld/src/com/edu/HelloWorld.java
@@ -1,9 +1,10 @@
 package com.edu;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-		System.out.println("배고파요!!");
+	public static void main(String[] args) {
+//      첫번째 프로그램입니다.		
 		System.out.println("점심 뭐먹지");
 		System.out.println("배고프다");
 	}
+
 }
