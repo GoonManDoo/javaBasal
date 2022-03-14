f50293a62d537e01ea1e57bd9211c98595cd80d8
PC <PC@DESKTOP-IQVPSQI>
2022-03-14 22:38:21 +0900

    03-14 숙제

---
+++ HelloWorld/src/com/edu/MathExample.java
@@ -0,0 +1,24 @@
+package com.edu;
+
+import java.util.Scanner;
+
+public class MathExample {
+	public static void main(String[] args) {
+	Scanner scn = new Scanner(System.in);
+	double num1, num2;
+	System.out.println("첫번째 숫자를 입력하세요..");
+	num1 = scn.nextInt(); //입력했던 값을 불러오는 기능
+	System.out.println("두번째 숫자를 입력하세요..");
+    num2 = scn.nextInt(); //입력했던 값을 불러오는 기능
+	
+	System.out.println("두수의 합? " + (num1 + num2));
+	System.out.println("두수의 차? " + (num1 - num2));
+	System.out.println("두수의 곱? " + (num1 * num2));
+	System.out.println("두수의 나누기? " + (int)(num1 / num2));
+	System.out.println("두수의 나머지? " + (num1 % num2));
+	
+	scn.close();
+	}
+	
+
+}
