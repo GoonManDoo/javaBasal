b78ad7e7b6fceff9aa674ca2b42c63afb92469a9
admin <admin@YD-13>
2022-03-14 11:11:04 +0900

    sedddd commit

--- HelloWorld/src/com/edu/HelloWorld.java
+++ HelloWorld/src/com/edu/HelloWorld.java
@@ -3,5 +3,7 @@ package com.edu;
 public class HelloWorld {
     public static void main(String[] args) {
 		System.out.println("배고파요!!");
+		System.out.println("점심 뭐먹지");
+		System.out.println("배고프다");
 	}
 }
