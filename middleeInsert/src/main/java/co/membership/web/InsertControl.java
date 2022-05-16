d4b34f0f08116e8bb1247650aefe95719e465dc1
KYM <kym8439@gmail.com>
2022-05-16 17:47:28 +0900

    05-16 학원 프로젝트 커밋

--- middleeInsert/src/main/java/co/membership/web/InsertControl.java
+++ middleeInsert/src/main/java/co/membership/web/InsertControl.java
@@ -3,8 +3,12 @@ package co.membership.web;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 import co.membership.service.MemberService;
 import co.membership.vo.MemberVO;
@@ -14,15 +18,24 @@ public class InsertControl implements Controller {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		int number = Integer.parseInt(request.getParameter("num"));
-		String name = request.getParameter("name");
-		int price = Integer.parseInt(request.getParameter("price"));
-		String category = request.getParameter("category");
-		String comment = request.getParameter("comment");
-		int sale = Integer.parseInt(request.getParameter("sale"));
-		String pimg = request.getParameter("pimg");
-		String gender = request.getParameter("gender");
-		String email = request.getParameter("email");
+		String saveDir = "upload";
+		saveDir = request.getServletContext().getRealPath(saveDir);
+		int maxSize = 1024 * 1024 * 10;
+		String encoding = "UTF-8";
+		// multipart 요청.
+		// request, 저장위치, 최대사이즈, 인코딩, 리네임정책. file.jpg, file1.jpg
+		MultipartRequest multi = new MultipartRequest(request, saveDir, maxSize, encoding,
+				new DefaultFileRenamePolicy());
+		
+		int number = Integer.parseInt(multi.getParameter("num"));
+		String name = multi.getParameter("name");
+		int price = Integer.parseInt(multi.getParameter("price"));
+		String category = multi.getParameter("category");
+		String comment = multi.getParameter("comment");
+		int sale = Integer.parseInt(multi.getParameter("sale"));
+		String pimg = multi.getParameter("pimg");
+		String gender = multi.getParameter("gender");
+		String email = multi.getParameter("email");
 		
 		MemberVO vo = new MemberVO();
 		vo.setProDuctNum(number);
@@ -34,7 +47,7 @@ public class InsertControl implements Controller {
 		vo.setpImg(pimg);
 		vo.setGender(gender);
 		vo.setEmail(email);
-		
+		System.out.println(pimg);
 		MemberService service = new MemberService();
 		service.insertMember(vo);
 		
@@ -42,5 +55,10 @@ public class InsertControl implements Controller {
 		// Dispatcher 객체 forward.
 		request.getRequestDispatcher("/index.jsp").forward(request, response);
 	}
+
+//	private ServletRequest getServletContext() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
 
 }
--- middleeInsert/src/main/webapp/view/insert.jsp
+++ middleeInsert/src/main/webapp/view/insert.jsp
@@ -10,16 +10,16 @@
 <body>
 
     <h3>상품등록</h3>
-    <form action="../insert.do" method="post">
+    <form action="../insert.do" method="post" enctype="multipart/form-data">
         상품번호: <input type="number" name="num"><br>
         상품이름: <input type="text" name="name"><br>
         상품가격: <input type="number" name="price"><br>
         카테고리: <input type="text" name="category"><br>
         내용: <input type="text" name="comment"><br>
         세일: <input type="number" name="sale"><br>s
-        사진: <input type="file" name="img"><br>
+        사진: <input type="file" name="pimg"><br>
         성별: <input type="text" name="gender"><br>
-        아이디: <input type="email" name="id"><br>
+        아이디: <input type="email" name="email"><br>
         <input type="submit" value="등록">
     </form>
     
